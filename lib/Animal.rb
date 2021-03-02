691f04cae2f7edf15b725d02bb0f1e338c3e944c
Susana <susana.m.vik@gmail.com>
2021-03-01 20:41:21 -0500

    commit tree 34bcd4354cb30a12bb3659098a454ce3e6ba82fd

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,10 @@
 class Animal
 
+    attr_reader :species, :weight, :nickname
+    
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+    end
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,33 @@
 class Zoo
+    
+    @@all = []
+    
+    def initialize(name, location)
+        @name = name
+        @location = location
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select {|animal| amimal.zoo == self}
+    end
+
+    def animal_species
+        animals.species.map {|specie| specie.zoo == self}
+    end
+
+    def find_by_species
+    end
+
+    def animal_nicknames
+    end
+
+    def self.find_by_location
+    end
 
 end
